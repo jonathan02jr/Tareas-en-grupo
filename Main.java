45927c9ff45fb25aa0fcaaf945e623395659b443
eguachamin <151896808+eguachamin@users.noreply.github.com>
2024-05-29 22:25:15 -0500

    cambiado por Eve

--- Main.java
+++ Main.java
@@ -1,3 +1,19 @@
+class Genealogia {
+    public String parentesco;
+    public String fecha_nac;
+    public int telefono;
+
+    public Genealogia (String parentesco, String fecha_nac, int telefono){
+        this.parentesco=parentesco;
+        this.fecha_nac=fecha_nac;
+        this.telefono=telefono;
+    }
+    public void imprimir_datos(){
+        System.out.println("Parentesco: "+parentesco);
+        System.out.println("Fecha de nacimiento: "+fecha_nac);
+        System.out.println("Teléfono: "+telefono);
+    }
+}
 public class Main {
     public static void main(String[] args){
         System.out.println("hola :)");
