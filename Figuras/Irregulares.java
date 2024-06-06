344f9473e72914b54853159debb406138bb0d106
POO <ariel.ashqui@epn.edu.ec>
2024-06-06 08:17:09 -0500

    Cambio Ariel

---
+++ Figuras/Irregulares.java
@@ -0,0 +1,10 @@
+package Figuras;
+
+public class Irregulares extends DosD {
+    public Irregulares() {
+    }
+
+    public Irregulares(int nLados, String nombre) {
+        super(nLados, nombre);
+    }
+}
