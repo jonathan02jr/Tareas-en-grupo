561d5afecc88eb1813091114a86351dc2ef8f1ed
POO <jonathanrm445@gmail.com>
2024-06-06 08:07:59 -0500

    subclse 3d completada -JR

---
+++ Figuras/Regulares.java
@@ -0,0 +1,13 @@
+package Figuras;
+
+class Regulares extends DosD {
+
+    public Regulares(int nLados,String nombre, double area, double perimetro){
+        super(nLados, nombre, area, perimetro);
+    }
+
+    @Override
+    public void mostrar_datos() {
+        super.mostrar_datos();
+    }
+}
