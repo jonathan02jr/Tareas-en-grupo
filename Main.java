485e2cd49c1750d4607e86071d7e6a1fb019b52a
eguachamin <151896808+eguachamin@users.noreply.github.com>
2024-06-05 23:26:24 -0500

    cambiado por Eve

--- Main.java
+++ Main.java
@@ -1,13 +1,15 @@
+//Se crea la clase Padre
 class Genealogia {
     public String parentesco;
     public String fecha_nac;
     public int telefono;
-
+    //Se crea el método constructor
     public Genealogia (String parentesco, String fecha_nac, int telefono){
         this.parentesco=parentesco;
         this.fecha_nac=fecha_nac;
         this.telefono=telefono;
     }
+    //Se crea un método para imprimir
     public void imprimir_datos(){
         System.out.println("Parentesco: "+parentesco);
         System.out.println("Fecha de nacimiento: "+fecha_nac);
