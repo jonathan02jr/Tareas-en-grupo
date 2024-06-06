1fe28d5aa7a82408c92c8df7524ed2e8b55ae3f6
POO <jonathanrm445@gmail.com>
2024-06-06 07:50:11 -0500

    subclse 3d completada -JR

--- Figuras/TresD.java
+++ Figuras/TresD.java
@@ -4,15 +4,20 @@ class TresD extends Figuras_Geometricas{
     public int nCaras;
     public double arista;
 
+
     public TresD(int nLados, String nombre, int nCaras, double arista){
         super(nLados, nombre);
         this.nCaras=nCaras;
         this.arista=arista;
     }
 
-    public double calcular_volumen(Double, int){
+    public double calcular_volumen(int nCaras, double arista){
         super.mostrar_datos();
         System.out.println("N° de caras: "+nCaras);
-        System.out.println("Arista: "+arista);
+        System.out.println("Logitud de arista: "+arista);
+
+        double volumen = Math.pow(arista,3);
+        System.out.println("El volumen del cubo es: "+volumen);
+        return volumen;
     }
 }
