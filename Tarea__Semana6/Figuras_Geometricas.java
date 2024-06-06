01847af7d5eb3cb8aabbac9343a21b871a8f82c1
Jonathan Ramirez <jonathanrm445@gmail.com>
2024-06-05 23:32:06 -0500

    Se añadio figuras geometricas -Jonathan Ramirez

---
+++ Tarea__Semana6/Figuras_Geometricas.java
@@ -0,0 +1,16 @@
+package Tarea__Semana6;
+
+public class Figuras_Geometricas {
+    public int nLados;
+    public String nombre;
+
+    public Figuras_Geometricas(int nLados, String nombre){
+        this.nLados = nLados;
+        this.nombre = nombre;
+    }
+
+    public void mostrar_datos(){
+        System.out.println("N° Lados: "+nLados);
+        System.out.println("Nombre: "+nombre);
+    }
+}
--- Tarea__Semana6/Main.java
+++ Tarea__Semana6/Main.java
@@ -1,4 +1,12 @@
 package Tarea__Semana6;
 
+//Fecha tarea: 05/06/2024
+
 public class Main {
+    public static void main(String[] args){
+
+        Figuras_Geometricas figura1 = new Figuras_Geometricas(4,"Cuadrado");
+        System.out.println("--- DATOS CLASE PADRE ---");
+        figura1.mostrar_datos();
+    }
 }
