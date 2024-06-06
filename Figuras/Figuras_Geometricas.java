67d8570da6c3ed464458dd5753865797b9119012
RodArielX <ariel.ashqui@epn.edu.ec>
2024-06-05 23:52:51 -0500

    Cambio Ariel

--- Figuras/Figuras_Geometricas.java
+++ Figuras/Figuras_Geometricas.java
@@ -15,3 +15,21 @@ public class Figuras_Geometricas {
         System.out.println("Nombre: "+nombre);
     }
 }
+
+class DosD extends Figuras_Geometricas{
+    public double area;
+    public double perimetro;
+
+    public DosD(int nLados,String nombre, double area, double perimetro){
+        super(nLados, nombre);
+        this.area = area;
+        this.perimetro = perimetro;
+    }
+
+    /*public double calcular_area(double){
+        return calcular_area();
+    }
+    public double calcular_perimetro(double){
+        return calcular_perimetro();
+    }*/
+}
