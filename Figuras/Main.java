06a2a78ea8c5f85537c1dda59c4e9e60fdee41bb
Jonathan Ramirez <jonathanrm445@gmail.com>
2024-06-12 22:46:31 -0500

    Se añade el menu - Jonathan Ramirez

--- Figuras/Main.java
+++ Figuras/Main.java
@@ -8,76 +8,111 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        // Menú
-        System.out.println("Seleccione la figura:");
-        System.out.println("1. Cuadrilátero");
-        System.out.println("2. Triángulo");
-        System.out.println("3. Cubo");
-        System.out.println("4. Otro");
-        int opcion = scanner.nextInt();
+        while (true){
+            System.out.println("1. Figura 2D");
+            System.out.println("2. Figura 3D");
+            System.out.println("3. Salir");
+            System.out.print("Seleccione el tipo de figura: ");
+            int tipoFigura = scanner.nextInt();
 
-        switch (opcion) {
-            case 1: // Cuadrilátero
-                System.out.println("Ingrese el lado A del cuadrilátero:");
-                double ladoA = scanner.nextDouble();
-                System.out.println("Ingrese el lado B del cuadrilátero:");
-                double ladoB = scanner.nextDouble();
-                Cuadrilateros cuadrilatero = new Cuadrilateros(ladoA, ladoB);
-                cuadrilatero.calcularArea();
-                cuadrilatero.calcularPerimetro();
-                cuadrilatero.mostrar_Datos();
-                break;
-            case 2: // Triángulo
-                System.out.println("Ingrese la base (Lado A) del triángulo:");
-                ladoA = scanner.nextDouble();
-                System.out.println("Ingrese la altura (Lado B)del triángulo:");
-                ladoB = scanner.nextDouble();
-                System.out.println("Ingrese el lado (C) del triángulo:");
-                double ladoC = scanner.nextDouble();
-                Triangulos triangulo = new Triangulos(ladoA, ladoB,ladoC);
-                triangulo.calcularArea();
-                triangulo.calcularPerimetro();
-                triangulo.mostrar_Datos();
-                break;
-            case 3:
-                System.out.println("Ingrese el número de caras del cubo:");
-                int nCaras = scanner.nextInt();
-                System.out.println("Ingrese la longitud de la arista del cubo:");
-                double arista = scanner.nextDouble();
-                TresD cubo = new TresD(6, "Cubo", nCaras, arista);
-                double volumenCubo = cubo.calcular_volumen(cubo.nCaras, cubo.arista);
-                System.out.println("Esta figura es en 3D");
-                break;
-            case 4: // Otro
-                System.out.println("Seleccione el tipo de figura:");
-                System.out.println("1. N Lados");
-                System.out.println("2. Círculo");
-                int subopcion = scanner.nextInt();
-                switch (subopcion) {
-                    case 1: // N Lados
-                        System.out.println("Ingrese la cantidad de lados del polígono:");
-                        int cantidadLados = scanner.nextInt();
-                        System.out.println("Ingrese la longitud de cada lado del polígono:");
-                        double longitudLado = scanner.nextDouble();
-                        NLados poligono = new NLados(cantidadLados, longitudLado);
-                        poligono.calcularArea();
-                        poligono.calcularPerimetro();
-                        poligono.mostrar_Datos();
-                        break;
-                    case 2: // Círculo
-                        System.out.println("Ingrese el radio del círculo:");
-                        double radio = scanner.nextDouble();
-                        Circulo circulo = new Circulo(radio);
-                        circulo.calcularArea();
-                        circulo.calcularPerimetro();
-                        circulo.mostrar_Datos();
-                        break;
-                    default:
-                        System.out.println("Opción no válida");
-                }
-                break;
-            default:
-                System.out.println("Opción no válida");
+            switch (tipoFigura) {
+                case 1:
+                    System.out.println("1. Cuadrilátero");
+                    System.out.println("2. Triángulo");
+                    System.out.println("3. Otro");
+                    System.out.println("Seleccione la figura: ");
+
+                    int opcion = scanner.nextInt();
+
+                    switch (opcion) {
+                        case 1: // Cuadrilátero
+                            System.out.println("Ingrese el lado A del cuadrilátero:");
+                            double ladoA = scanner.nextDouble();
+                            System.out.println("Ingrese el lado B del cuadrilátero:");
+                            double ladoB = scanner.nextDouble();
+                            Cuadrilateros cuadrilatero = new Cuadrilateros(ladoA, ladoB);
+                            cuadrilatero.calcularArea();
+                            cuadrilatero.calcularPerimetro();
+                            cuadrilatero.mostrar_Datos();
+                            break;
+                        case 2: // Triángulo
+                            System.out.println("Ingrese la base (Lado A) del triángulo:");
+                            ladoA = scanner.nextDouble();
+                            System.out.println("Ingrese la altura (Lado B) del triángulo:");
+                            ladoB = scanner.nextDouble();
+                            System.out.println("Ingrese el lado (C) del triángulo:");
+                            double ladoC = scanner.nextDouble();
+                            Triangulos triangulo = new Triangulos(ladoA, ladoB, ladoC);
+                            triangulo.calcularArea();
+                            triangulo.calcularPerimetro();
+                            triangulo.mostrar_Datos();
+                            break;
+                        case 3: // Otro
+                            System.out.println("Seleccione el tipo de figura:");
+                            System.out.println("1. N Lados");
+                            System.out.println("2. Círculo");
+                            int subopcion = scanner.nextInt();
+                            switch (subopcion) {
+                                case 1: // N Lados
+                                    System.out.println("Ingrese la cantidad de lados del polígono:");
+                                    int cantidadLados = scanner.nextInt();
+                                    System.out.println("Ingrese la longitud de cada lado del polígono:");
+                                    double longitudLado = scanner.nextDouble();
+                                    NLados poligono = new NLados(cantidadLados, longitudLado);
+                                    poligono.calcularArea();
+                                    poligono.calcularPerimetro();
+                                    poligono.mostrar_Datos();
+                                    break;
+                                case 2: // Círculo
+                                    System.out.println("Ingrese el radio del círculo:");
+                                    double radio = scanner.nextDouble();
+                                    Circulo circulo = new Circulo(radio);
+                                    circulo.calcularArea();
+                                    circulo.calcularPerimetro();
+                                    circulo.mostrar_Datos();
+                                    break;
+                                default:
+                                    System.out.println("Opción no válida");
+                            }
+                            break;
+                        default:
+                            System.out.println("Opción no válida");
+                    }
+                    break;
+
+                case 2: // Figura 3D
+                    System.out.println("1. Cubo");
+                    System.out.println("2. Esfera");
+                    System.out.print("Seleccione la figura 3D: ");
+
+                    int opcion3D = scanner.nextInt();
+
+                    switch (opcion3D) {
+                        case 1:
+                            System.out.println("Ingrese la longitud de la arista del cubo:");
+                            double aristaCubo = scanner.nextDouble();
+                            // volumen del cubo
+                            double volumenCubo = Math.pow(aristaCubo, 3);
+                            System.out.println("Volumen del cubo: " + volumenCubo+'\n');
+                            break;
+                        case 2:
+                            System.out.println("Ingrese el radio de la esfera:");
+                            double radioEsfera = scanner.nextDouble();
+                            double volumenEsfera = (4.0 / 3.0) * Math.PI * Math.pow(radioEsfera, 3);
+                            System.out.println("Volumen de la esfera: " + Math.round(volumenEsfera)+'\n');
+                            break;
+                        default:
+                            System.out.println("Opción no válida");
+                    }
+                    break;
+                case 3:
+                    System.out.println("Saliendo del sistema....");
+                    return;
+                default:
+                    System.out.println("Opción no válida");
+            }
+
         }
+
     }
 }
