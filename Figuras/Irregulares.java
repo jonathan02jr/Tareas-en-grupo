9c14089f1f07d7f0e4ac56e47f380c1635aa8381
RodArielX <ariel.ashqui@epn.edu.ec>
2024-06-12 23:11:03 -0500

    Cambio Ariel

--- Figuras/Irregulares.java
+++ Figuras/Irregulares.java
@@ -1,10 +1,22 @@
 package Figuras;
 
 public class Irregulares extends DosD {
-    public Irregulares() {
+    private double ladoA;
+    private double ladoB;
+    private double ladoC;
+    private double ladoD;
+
+    public Irregulares() {}
+
+    public Irregulares(int nLados, String nombre, double ladoA, double ladoB, double ladoC, double ladoD) {
+        super(nLados, nombre);
+        this.ladoA = ladoA;
+        this.ladoB = ladoB;
+        this.ladoC = ladoC;
+        this.ladoD = ladoD;
     }
 
-    public Irregulares(int nLados, String nombre) {
-        super(nLados, nombre);
+    public boolean esIrregular() {
+        return ladoA != ladoB || ladoB != ladoC || ladoC != ladoD;
     }
 }
--- Figuras/Regulares.java
+++ Figuras/Regulares.java
@@ -1,10 +1,22 @@
 package Figuras;
 
 public class Regulares extends DosD {
-    public Regulares() {
+    private double ladoA;
+    private double ladoB;
+    private double ladoC;
+    private double ladoD;
+
+    public Regulares() {}
+
+    public Regulares(int nLados, String nombre, double ladoA, double ladoB, double ladoC, double ladoD) {
+        super(nLados, nombre);
+        this.ladoA = ladoA;
+        this.ladoB = ladoB;
+        this.ladoC = ladoC;
+        this.ladoD = ladoD;
     }
 
-    public Regulares(int nLados, String nombre) {
-        super(nLados, nombre);
+    public boolean esRegular() {
+        return ladoA == ladoB && ladoB == ladoC && ladoC == ladoD;
     }
 }
